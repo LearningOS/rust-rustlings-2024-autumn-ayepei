99513ec8f46ef51fccee801a30e56b6bff35d2c6
ayepei <ayepei>
2024-10-12 19:45:23 +0800

    finish test

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -13,17 +13,21 @@ pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
 
+pub fn is_odd(num: i32) -> bool {
+    num % 2 == 1
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn is_true_when_even() {
-        assert!(is_even(4),true);
+        assert!(is_even(4));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!(is_even(5),false);
+        assert!(is_odd(5));
     }
 }
