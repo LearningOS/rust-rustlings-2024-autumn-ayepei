db9a36613da48eacc227e733828c538403b22044
ayepei <ayepei>
2024-10-12 20:49:46 +0800

    finish quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,7 +16,7 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 pub struct ReportCard {
     pub grade: f32,
@@ -28,6 +28,19 @@ impl ReportCard {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+    }
+}
+
+pub struct Alphabetical {
+    pub grade: f32,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl Alphabetical {
+    pub fn print(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, "A+")
     }
 }
 
@@ -51,7 +64,7 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
+        let report_card = Alphabetical {
             grade: 2.1,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
